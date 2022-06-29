60ba2abb227741b78716d31cedcaacbac40ed2e1
kmzr34 <mohamedkhairlow@gmail.com>
2022-06-29 22:37:00 +0300

    Print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = number % 10
-    print(last_digit)
+    if number > 0:
+        last_digit = number % 10
+        print(last_digit)
+    elif number < 0:
+        last_digit = number % -10
+        print(last_digit)
+    else:
+        print(0)
+
+
