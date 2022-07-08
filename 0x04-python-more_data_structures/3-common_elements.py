d9a2d4f56434acf12ef2854412a696aa7235bfc5
kmzr34 <mohamedkhairlow@gmail.com>
2022-07-08 22:56:49 +0300

    Common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-# create a function that returns a set of all elements present in only one set.
-def only_diff_elements(set_1, set_2):
-    return set_1 ^ set_2
+# Write a function that returns a set of common elements in two sets.
+def common_elements(set_1, set_2):
+    return set_1 & set_2
